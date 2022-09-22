157fa95206038bf4b414f2f4b104538add490a8b
chainsaw2k <tom.mccollough@section.io>
2022-09-21 20:35:28 -0600

    1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.json({
     commit_id: process.env.COMMIT_ID || 'unknown',
-    port
+    port, 1
   })
 })
 
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -23,7 +23,7 @@ setCluster() {
   --server=$KEI_URL \
   --certificate-authority=$cert
 
-  kubectl config set-credentials section-user --token=$KEI_USER_TOKEN
+  kubectl config set-credentials section-user --token=$SECTION_API_TOKEN
 
   kubectl config set-context my-section-application --cluster=section --user=section-user --namespace=default
 
