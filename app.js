42d779d12004b9a7b6b13b4ff1d287726564f0a2
chainsaw2k <tom.mccollough@section.io>
2022-10-03 14:16:14 -0600

    add a hello world msg

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.json({
-    commit_id: process.env.COMMIT_ID || 'unknown2',
-    port,
-    foo:6
+    "Hello world from Node Express!\nThis app is being served from 4 locations around the world: New York, SFO, Amsterdam, Sydney.\nUse "dig <appname>" to see which one you are hitting.\nDeploy your own Node Express app with this same capability by visiting http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\n\n",
+    commit_id: process.env.COMMIT_ID || 'unknown',
+    port
   })
 })
 
