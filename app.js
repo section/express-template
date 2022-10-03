70db5837d96b94b09dd9b459a4509196556f2a34
chainsaw2k <tom.mccollough@section.io>
2022-10-03 14:19:19 -0600

    better msg

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.json({
-    "Hello world from Node Express!\nThis app is being served from 4 locations around the world: New York, SFO, Amsterdam, Sydney.\nUse "dig <appname>" to see which one you are hitting.\nDeploy your own Node Express app with this same capability by visiting http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\n\n",
+    "Hello world from Node Express!\n\nThis app is being served from 4 locations around the world for low latency and high availability: New York, SFO, Amsterdam, Sydney.\n\nUse:\n$ dig floral-smoke-4415.section.app\nto see which location you are hitting.\n\nDeploy your own Node Express app with this same capability by visiting http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\n\n",
     commit_id: process.env.COMMIT_ID || 'unknown',
     port
   })
