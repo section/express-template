b07acb3c5e2a053f6b6c5e5ab6a2c4e1f5c1dc69
chainsaw2k <tom.mccollough@section.io>
2022-10-03 15:29:37 -0600

    msg

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send(
-    "<h1>Hello World from Node Express!</h1><p>This app is being served from 4 locations around the world for low latency and high availability: New York, SFO, Amsterdam, Sydney.</p><p>Use: dig floral-smoke-4415.section.app</br>to see which location you are hitting (look in the ANSWER SECTION).</p><p>Deploy your own Node Express app with this same capability by visiting <a href=\"http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\">http://beta.section.io/docs/tutorials/frameworks/nodeexpress/</a></p>",
+    "<h1>Hello World from Node Express on Section!</h1><p>This app is being served from 4 locations around the world for low latency and high availability: New York, SFO, Amsterdam, Sydney.</p>\
+    <p>To see which location you are hitting use the dig command.</p>\
+    <p>dig floral-smoke-4415.section.app</br>(look in the ANSWER section)</p>\
+    <p>Deploy your own Node Express app with this same capability by visiting <a href=\"http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\">http://beta.section.io/docs/tutorials/frameworks/nodeexpress/</a></p>",
     );
 })
 
