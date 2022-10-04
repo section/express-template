165b951fa601111adc1abb1d6a7a4b8ef9503ccf
chainsaw2k <tom.mccollough@section.io>
2022-10-04 09:59:31 -0600

    upgrade github token attempt

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send(
-    "<h1>Hello World from Node Express on Section!</h1><p>This app is being served from 4 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>\
+    "<h1>YAY 1 Hello World from Node Express on Section!</h1><p>This app is being served from 4 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>\
     <p>To see which location you are hitting use the dig command.</p>\
     <p>dig floral-smoke-4415.section.app</br>(look in the ANSWER section)</p>\
     <p>Deploy your own Node Express app with this same capability by visiting <a href=\"http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\">http://beta.section.io/docs/tutorials/frameworks/nodeexpress/</a></p>",
