d1f4a4c6848dce4a09ff4384e1c29a9ecac36928
chainsaw2k <tom.mccollough@section.io>
2022-10-05 12:48:09 -0600

    bug

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send(
-    "<head><style>body { font-family: "Segoe UI"; }</style></head>
+    "<head><style>body { font-family: \"Segoe UI\"; }</style></head>\
     }<h1>Hello World from Node Express on Section!!!</h1><p>This app is being served from 4 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>\
     <p>To see which location you are hitting use the dig command.</p>\
     <p>dig floral-smoke-4415.section.app</br>(look in the ANSWER section)</p>\
