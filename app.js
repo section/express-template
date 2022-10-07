3e05dc1c62e8f477810f4ed9e8f993f3b3631506
chainsaw2k <tom.mccollough@section.io>
2022-10-07 08:05:52 -0600

    no longer beta

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     <h1>Hello World from Node Express on Section!!!</h1><p>This app is being served from 4 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>\
     <p>Discover the location you are hitting with the dig command.</p>\
     <p>dig floral-smoke-4415.section.app</br>(look in the ANSWER section for a 3 letter city code)</p>\
-    <p>Distribute your own Node Express app by visiting <a href=\"http://beta.section.io/docs/tutorials/frameworks/nodeexpress/\">http://beta.section.io/docs/tutorials/frameworks/nodeexpress/</a></p>",
+    <p>Distribute your own Node Express app by visiting <a href=\"http://section.io/docs/tutorials/frameworks/nodeexpress/\">http://beta.section.io/docs/tutorials/frameworks/nodeexpress/</a></p>",
     );
 })
 
