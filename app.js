0318ce81c5e3cf117b7433e3a255b83a40ca7c65
CJ Brewer <brewercalvinj@gmail.com>
2022-10-10 11:57:43 -0600

    Add location info

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+const fetch = require('node-fetch')
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const response = await fetch('https://ipinfo.io/json');
+  const info = await response.text();
+  const locationInfo = JSON.parse(info);
+
   res.send(
-    "<head><style>body { font-family: \"Segoe UI\"; }</style></head>\
+    `<head><style>body { font-family: \"Segoe UI\"; }</style></head>\
     <h1>Hello World from Node Express on Section!!!</h1><p>This app is being served from 4 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>\
-    <p>Discover the location you are hitting with the dig command.</p>\
-    <p>dig floral-smoke-4415.section.app</br>(look in the ANSWER section for a 3 letter city code)</p>\
-    <p>Distribute your own Node Express app by visiting <a href=\"http://section.io/docs/tutorials/frameworks/nodeexpress/\">http://section.io/docs/tutorials/frameworks/nodeexpress/</a></p>",
+    <p>Below you will find the information where this application is running in the world.</p>\
+    <p>You are hitting the example application running in ${locationInfo.city}, ${locationInfo.region} - ${locationInfo.country}</p>\
+    <p>Distribute your own Node Express app by visiting <a href=\"http://section.io/docs/tutorials/frameworks/nodeexpress/\">http://section.io/docs/tutorials/frameworks/nodeexpress/</a></p>`,
     );
 })
 
