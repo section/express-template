bb5f98d43a214a0c843c2b08e6f3142f6e92b626
Tom McCollough <35183135+chainsaw2k@users.noreply.github.com>
2022-10-18 12:30:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', async (req, res) => {
 
   res.send(
     `<head><style>body { font-family: "Segoe UI"; }</style></head>
-    <h1>Hello World from Node Express on Section!!!</h1><p>This app is being served from 4 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>
+    <h1>Hello World from Node Express on Section!!!</h1><p>This app is being served from 2 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO.</p>
     <p>You are hitting the example application running in ${locationInfo.city}, ${locationInfo.region} - ${locationInfo.country}</p>
     <p>Distribute your own Node Express app by visiting <a href=\"http://section.io/docs/tutorials/frameworks/nodeexpress/\">http://section.io/docs/tutorials/frameworks/nodeexpress/</a></p>`
     );
